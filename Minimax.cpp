255ae7e6fdf8f1978e44ee83ce1fc729fc61bc0a
Pranav C <xtreme5351@gmail.com>
2023-01-30 12:23:27 +0000

    Minimax works, now just need the opponent to actually place pieces

--- Minimax.cpp
+++ Minimax.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <vector>
 #include "Logic.h"
+#include "IO.h"
 using namespace std;
 
 int Minimax::weightWinCons() const {
@@ -23,12 +24,44 @@ int Minimax::weightWinCons() const {
 vector<vector<int>> Minimax::runMinimax(int player) {
     int result = this->minimax(0, false, player);
     vector<vector<int>> temp;
+    cout << "@" << result << endl;
     return temp;
 }
 
 int Minimax::minimax(int depth, bool isMax, int player) {
     int state = weightWinCons();
-    return state;
+//    IO::printBoard(this->board);
+    if (state == 10 || state == -10) {
+        return state;
+    } else if (!this->movesLeft()) {
+        return 0;
+    }
+
+    if (isMax){
+        int best = -1000;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (this->board[i][j]== 0) {
+                    this->board[i][j] = player;
+                    best = max(best, minimax(depth+1, !isMax, player));
+                    this->board[i][j] = 0;
+                }
+            }
+        }
+        return best;
+    } else {
+        int best = 1000;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (this->board[i][j]== 0) {
+                    this->board[i][j] = 1;
+                    best = min(best, minimax(depth+1, !isMax, player));
+                    this->board[i][j] = 0;
+                }
+            }
+        }
+        return best;
+    }
 }
 
 bool Minimax::movesLeft() {
